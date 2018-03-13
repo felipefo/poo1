8b1d6ee1bb70dc4c7654d5ec8284368be2215711
felipefo <felipefo@users.noreply.github.com>
2018-03-13 10:01:22 -0300

    .
    .

--- Exemplos/JogoPerguntasERespostas_soma_subtracao_multiplicacao/src/jogo/pontuacao/Jogo.java
+++ Exemplos/JogoPerguntasERespostas_soma_subtracao_multiplicacao/src/jogo/pontuacao/Jogo.java
@@ -3,25 +3,24 @@ package jogo.pontuacao;
 
 import javax.swing.JOptionPane;
 
-public class Jogo {
 
-    public static void main(String[] args) {
-        
-        Pontuacao pontuacao = new Pontuacao();
-        
+public class Jogo {
+    
+    Pontuacao pontuacao = new Pontuacao();    
+    
+    public void iniciaJogo(){        
         for(;;){        
             QuestaoSoma novaQuestao = new QuestaoSoma();
             novaQuestao.criarPergunta();
             novaQuestao.exibirResultado();            
             pontuacao.verificarQuestao(novaQuestao);
             pontuacao.exibirPontuacao();                                    
-
             String continuar = JOptionPane.showInputDialog("Deseja continuar?(S)Sim ou (N)Não");
             if(continuar.equalsIgnoreCase("N")){
                 break;
             }                        
-        }
-                
+        }        
     }
+    
     
 }
---
+++ Exemplos/JogoPerguntasERespostas_soma_subtracao_multiplicacao/src/jogo/pontuacao/Main.java
@@ -0,0 +1,11 @@
+
+package jogo.pontuacao;
+
+public class Main {
+        
+    public static void main(String[] args) {        
+        Jogo jogoQuestoes = new Jogo();
+        jogoQuestoes.iniciaJogo();                
+    }
+    
+}
